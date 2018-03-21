81ecacf1378a4473160c60c75608d63f546a41e3
daniluk4000 <daniluk4000@outlook.com>
2018-03-21 03:15:51 +0300

    Projects page v0.1
    - Filter made
    - Effects & sample projects list made
    
    TODO: page nav

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-    $('.print').click(function() {
+$(document).ready(function () {
+    $('.print').click(function () {
         window.print();
+    });
+    $('.project-open').click(function () {
+        $('.projects-menu-item').removeClass('active');
+        $(this).addClass('active');
+        if ($(this).hasClass('open-inet')) {
+            $('.type-inet').show('slow');
+           $('.type-org').hide('slow');
+        } else if ($(this).hasClass('open-org')) {
+            $('.type-org').show('slow');
+            $('.type-inet').hide('slow');
+        } else {
+            $('.project').show('slow');
+        }
     });
 });
